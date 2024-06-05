a52abdd9d29315e0e3cf956bfe88a86d3a45673c
Vitinh021 <victormarcos857@gmail.com>
2024-06-05 08:49:46 -0300

    iniciando projeto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
+
 export class Tab1Page {
 
-  constructor() {}
+
+/*   goToTab2() {
+    this.router.navigateByUrl('/tabs/tab2');
+  } */
+
+  ok(){
+    this.storage.set('valor', "aaaaa");
+  }
+
+  ok2(){
+    this.storage.get('valor')
+    .then((resposta : any) => {
+      console.log(resposta);
+    })
+    .catch((erro : any)=>{
+      console.log("Error: " + erro);
+    })
+  }
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init(){
+    this.storage = await this.storage.create();
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,86 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  terefas = new Array<Tarefa>;
+  titulo !: string;
+  descricao!: string;
+  dtInicio!: Date;
+  dtEntrega!: Date;
+  isConcluido!: Boolean;
+  categoria!: String;
+  private chaveTarefas = 'tarefas';
 
   constructor() {}
+
+}
+
+export class Tarefa {
+
+  private titulo: string;
+  private descricao: string;
+  private dtInicio: Date;
+  private dtEntrega: Date;
+  private isConcluido: Boolean;
+  private categoria: String;
+
+  constructor(tit: string, desc: string, dtIni :Date, dtEnt :Date, isCon: Boolean, cate: String) {
+    this.titulo = tit;
+    this.descricao = desc;
+    this.dtInicio = dtIni;
+    this.dtEntrega = dtEnt;
+    this.isConcluido = isCon;
+    this.categoria = cate;
+  }
+
+    // Getters
+    public getTitulo(): string {
+      return this.titulo;
+    }
+
+    public getDescricao(): string {
+      return this.descricao;
+    }
+
+    public getDtInicio(): Date {
+      return this.dtInicio;
+    }
+
+    public getDtEntrega(): Date {
+      return this.dtEntrega;
+    }
+
+    public getIsConcluido(): Boolean {
+      return this.isConcluido;
+    }
+
+    public getCategoria(): String {
+      return this.categoria;
+    }
+
+    // Setters
+    public setTitulo(titulo: string): void {
+      this.titulo = titulo;
+    }
+
+    public setDescricao(descricao: string): void {
+      this.descricao = descricao;
+    }
+
+    public setDtInicio(dtInicio: Date): void {
+      this.dtInicio = dtInicio;
+    }
+
+    public setDtEntrega(dtEntrega: Date): void {
+      this.dtEntrega = dtEntrega;
+    }
+
+    public setIsConcluido(isConcluido: Boolean): void {
+      this.isConcluido = isConcluido;
+    }
+
+    public setCategoria(categoria: String): void {
+      this.categoria = categoria;
+    }
+
 
 }
